85932f39c6a9c50adf8354942625c8365e6e1343
jovancha <jovancha11@gmail.com>
2022-04-27 18:57:36 +0200

    edited gradle build

--- src/test/java/com/cucumbertest/cucumber/runConfiguration/RunConfiguration.java
+++ src/test/java/com/cucumbertest/cucumber/runConfiguration/RunConfiguration.java
@@ -19,15 +19,8 @@ public class RunConfiguration {
         return defaultBrowser;
     }
 
-    public void setDefaultBrowser(String defaultBrowser) {
-        this.defaultBrowser = defaultBrowser;
-    }
-
     public String getSecondBrowser() {
         return secondBrowser;
     }
 
-    public void setSecondBrowser(String secondBrowser) {
-        this.secondBrowser = secondBrowser;
-    }
 }
